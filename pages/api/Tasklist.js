c98984db8e18d067979470b63765a85a0be190a0
Renan_Brandao <104158547+Durvaks@users.noreply.github.com>
2023-05-06 09:10:19 -0300

    alterações iniciais

---
+++ pages/api/Tasklist.js
@@ -0,0 +1,15 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+   try {
+       const client = await clientPromise;
+       const db = client.db("test");
+       const tasks = await db //<== continuar a partir daqui!
+           .collection("tasks")
+           .find({})
+           .toArray();
+      res.json(movies);
+   } catch (e) {
+       console.error(e);
+   }
+};
---
+++ pages/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import clientPromise from '../lib/mongodb'
+import { InferGetServerSidePropsType } from 'next'
+
+export async function getServerSideProps() {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+export default function Home({
+  isConnected,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <a href="/api/movies">Acessar Tasks</a>
+        <form action="">
+          <input type="text" name="test" id="test" />
+          <button type="submit">Acessar</button>
+        </form>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
